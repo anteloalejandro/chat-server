782874ae7c6b8fd3f65e3a6e12363ebcb9114ddb
Alejandro Antelo <alfasant@protonmail.com>
2023-03-03 00:19:28 +0100

    basic auth

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const User = require('../models/user.js')
+const bcrypt = require('bcrypt')
 
 const fileConfig = { root: 'views' }
 
@@ -12,15 +13,35 @@ router.get('/list-users', (req, res) => {
 })
 
 router.post('/sign-up', (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  })
+  const saltRounds = 10;
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    if (err)
+      throw new Error('Internal Server Error while hashing password')
 
-  user.save()
-    .then(response => { res.send({error: false, msg: response}) })
-    .catch(error => { res.send({error: true, msg: error}) })
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    })
+
+    user.save()
+      .then(response => { res.send({error: false, msg: response}) })
+      .catch(error => { res.send({error: true, msg: error}) })
+
+  })
+})
+
+router.post('/sign-in', (req, res) => {
+  User.findOne({email: req.body.email})
+    .then(user => {
+      console.log(user)
+      bcrypt.compare(req.body.password, user.password, (err, match) => {
+        if (match)
+          res.send({error: false, msg: 'Successful log in'})
+        else
+          res.send({error: true, msg: 'email, username or password are wrong'})
+      })
+    })
 })
 
 router.post('/delete-account', (req, res) => {
