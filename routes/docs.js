edd1ec9051897097dc38794a015cd89d2b55244b
Alejandro Antelo <alfasant@protonmail.com>
2023-03-12 03:11:11 +0100

    added documentation routes and resources

--- routes/docs.js
+++ routes/docs.js
@@ -4,3 +4,13 @@ export const router = Router()
 router.get('/', (req, res) => {
   res.render('index')
 })
+
+router.get('/users', (req, res) => {
+  res.render('users')
+})
+router.get('/messages', (req, res) => {
+  res.render('messages')
+})
+router.get('/conversations', (req, res) => {
+  res.render('conversations')
+})
