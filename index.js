a7e40eeaec7596c540edb6179c48af86c47941c8
Alejandro Antelo <alfasant@protonmail.com>
2023-05-28 08:02:45 +0200

    prevent crashes when an error happens

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ import { User } from './models/user.js'
 import { Message } from './models/message.js'
 import { Conversation } from './models/conversation.js'
 import { encryptUserData } from './encrypt.js'
+process.on('uncaughtException', err => {
+  console.log({uncaughtException: err})
+})
 const app = express()
 
 hbs.registerPartials('./docs/partials');
